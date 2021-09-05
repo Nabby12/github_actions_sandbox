fdd6129d349df3ebb8e6c99b4cb090f91bfd978d
Nabby12 <Nabby12@users.noreply.github.com>
2021-09-06 05:07:36 +0900

    feature/added-shell-action

---
+++ .github/actions/shell_action/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sample_input="$INPUT_SAMPLE_INPUT"
+
+echo $sample_input
+
+SAMPLE_OUTPUT="${sample_input}"" -> sample_output"
+
+echo "::set-output name=sample_output::$SAMPLE_OUTPUT"
