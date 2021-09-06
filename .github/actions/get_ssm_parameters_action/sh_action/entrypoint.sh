c8c91d9a6d085d6067a8cad452b7677dd60bb4ed
Nabby12 <Nabby12@users.noreply.github.com>
2021-09-06 15:08:13 +0900

    modified/entrypoint-sh

--- .github/actions/get_ssm_parameters_action/sh_action/entrypoint.sh
+++ .github/actions/get_ssm_parameters_action/sh_action/entrypoint.sh
@@ -10,13 +10,13 @@ ARRAY_COUNT=`expr "${#PARAMS_ARRAY[*]}"`
 echo "---"
 echo "${ARRAY_COUNT}"
 
-SSM_PARAMETERS="'{"
+SSM_PARAMETERS="{"
 i=1
 for param in "${PARAMS_ARRAY[@]}"
 do
     END_STRING=","
     if [ "${i}" -eq "${ARRAY_COUNT}" ]; then
-        END_STRING="}'"
+        END_STRING="}"
     fi
 
     SSM_PARAMETERS="${SSM_PARAMETERS}"\""${param}"\"\:\""${param}"\""${END_STRING}"
