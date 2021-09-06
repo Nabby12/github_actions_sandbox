102e129337ca94d120bb50abb38d3bdca3d6420a
Nabby12 <Nabby12@users.noreply.github.com>
2021-09-06 16:06:54 +0900

    modified/entrypoint-sh

--- .github/actions/get_ssm_parameters_action/sh_action/entrypoint.sh
+++ .github/actions/get_ssm_parameters_action/sh_action/entrypoint.sh
@@ -8,9 +8,8 @@ IFS=, PARAMS_ARRAY=(${sample_input_args})
 ARRAY_COUNT=`expr "${#PARAMS_ARRAY[*]}"`
 
 SSM_PATH="/test_path/"
-
 aws ssm get-parameter \
-    --name `"${SSM_PATH}"TEST_VALUE1` \
+    --name  $("${XXX}"TEST_VALUE1) \
     --with-decryption
 
 i=1
