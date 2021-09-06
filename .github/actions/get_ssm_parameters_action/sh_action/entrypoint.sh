d67c719988819ea1ffb7b60c05241072b9eed031
Nabby12 <Nabby12@users.noreply.github.com>
2021-09-06 14:08:40 +0900

    added/sh-action

---
+++ .github/actions/get_ssm_parameters_action/sh_action/entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/sh -l
+
+# input args
+sample_input_args="$INPUT_SAMPLE_INPUT_args"
+
+"$(sample_input_args)" | sed -z 's/\n/,/g;'
+
+echo "$(sample_input_args)"
+
+IFS=, PARAMS_ARRAY="(${sample_input_args})"
+
+SSM_PARAMETERS="'{"
+i=1
+for param in "${PARAMS_ARRAY[@]}"
+do
+    END_STRING=","
+    if [ i -eq "${#PARAMS_ARRAY[*]}" ]; then
+        END_STRING="}'"
+    fi
+
+    SSM_PARAMETERS="${SSM_PARAMETERS}"\""${param}"\"\:\""${param}"\""${END_STRING}"
+    let i++
+done
+
+SSM_PARAMETERS="${SSM_PARAMETERS}""}"
+
+# output arg
+echo "::set-output name=ssm_parameters::$SSM_PARAMETERS"
