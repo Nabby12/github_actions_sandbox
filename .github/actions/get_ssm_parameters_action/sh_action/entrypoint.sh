97ee0d5b34e1696558cf4d1bd067ef6fd49776ea
Nabby12 <Nabby12@users.noreply.github.com>
2021-09-06 15:00:19 +0900

    modified/entrypoint-sh

--- .github/actions/get_ssm_parameters_action/sh_action/entrypoint.sh
+++ .github/actions/get_ssm_parameters_action/sh_action/entrypoint.sh
@@ -6,24 +6,22 @@ sample_input_args=$(echo -n "${sample_input_args}" | sed --null-data -e 's/\n/,/
 
 IFS=, PARAMS_ARRAY=(${sample_input_args})
 
-echo '---'
-echo "${PARAMS_ARRAY[*]}"
-echo "${#PARAMS_ARRAY[*]}"
-echo '---'
-
 ARRAY_COUNT=`expr "${#PARAMS_ARRAY[*]}"`
+echo "---"
+echo "${ARRAY_COUNT}"
 
 SSM_PARAMETERS="'{"
 i=1
 for param in "${PARAMS_ARRAY[@]}"
 do
     END_STRING=","
-    if [ i -eq "${ARRAY_COUNT}" ]; then
+    if [ i -eq ${ARRAY_COUNT} ]; then
         END_STRING="}'"
     fi
 
     SSM_PARAMETERS="${SSM_PARAMETERS}"\""${param}"\"\:\""${param}"\""${END_STRING}"
     let i++
+    echo "${i}"
 done
 
 SSM_PARAMETERS="${SSM_PARAMETERS}""}"
