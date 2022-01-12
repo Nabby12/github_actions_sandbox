f3411b88bf76f3739611c0792567e646d4c422a0
Nabby12 <Nabby12@users.noreply.github.com>
2022-01-12 17:09:36 +0900

    modified/env-for-sh-action

--- .github/actions/get_ssm_parameters_action/sh_action/entrypoint.sh
+++ .github/actions/get_ssm_parameters_action/sh_action/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # input args
-default_region="DEFAULT_REGION"
-ssm_path_name="SSM_PATH_NAME"
-env_name="ENV_NAME"
+default_region="$DEFAULT_REGION"
+ssm_path_name="$SSM_PATH_NAME"
+env_name="$ENV_NAME"
 
 cd_parameters="$CD_PARAMETERS"
 cd_parameters=$(echo -n "${CD_PARAMETERS}" | sed --null-data -e 's/\n/,/g;')
@@ -58,3 +58,45 @@ done
 
 # output arg
 echo "::set-output name=ssm_parameters::$SSM_PARAMETERS"
+
+
+
+
+
+
+
+
+
+
+# # input args
+# sample_input_args="$INPUT_SAMPLE_INPUT_ARGS"
+# sample_input_args=$(echo -n "${sample_input_args}" | sed --null-data -e 's/\n/,/g;')
+
+# IFS=, PARAMS_ARRAY=(${sample_input_args})
+# ARRAY_COUNT=`expr "${#PARAMS_ARRAY[*]}"`
+
+# SSM_PATH="/test_path/"
+
+# i=1
+# for param in "${PARAMS_ARRAY[@]}"
+# do
+#     END_STRING=","
+#     if [ "${i}" -eq 1 ]; then
+#         SSM_PARAMETERS="{"
+#     elif [ "${i}" -eq "${ARRAY_COUNT}" ]; then
+#         END_STRING="}"
+#     fi
+
+#     TARGET_KEY="${SSM_PATH}""${param}"
+#     RESPONSE=$(aws ssm get-parameter --name "${TARGET_KEY}" --with-decryption)
+#     VALUE="$(jq -r '.Parameter.Value' <(echo "${RESPONSE}"))"
+
+#     SSM_PARAMETERS="${SSM_PARAMETERS}"\""${param}"\"\:\""${VALUE}"\""${END_STRING}"
+#     let i++
+# done
+
+# # mask arg
+# echo "::add-mask::$SSM_PARAMETERS"
+
+# # output arg
+# echo "::set-output name=ssm_parameters::$SSM_PARAMETERS"
