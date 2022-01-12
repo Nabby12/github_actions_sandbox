172bebf2624319672c93e2b1ae29c6a117920b67
Nabby12 <Nabby12@users.noreply.github.com>
2022-01-12 17:24:44 +0900

    added/echo-for-sh-action

--- .github/actions/get_ssm_parameters_action/sh_action/entrypoint.sh
+++ .github/actions/get_ssm_parameters_action/sh_action/entrypoint.sh
@@ -60,15 +60,7 @@ done
 # output arg
 echo "::set-output name=ssm_parameters::$SSM_PARAMETERS"
 
-
-
-
-
-
-
-
-
-
+echo "${SSM_PARAMETERS}"
 # # input args
 # sample_input_args="$INPUT_SAMPLE_INPUT_ARGS"
 # sample_input_args=$(echo -n "${sample_input_args}" | sed --null-data -e 's/\n/,/g;')
