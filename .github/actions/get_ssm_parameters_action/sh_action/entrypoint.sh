cda4af0bdabc9d4605bd74da64e3754bcc056a95
Nabby12 <Nabby12@users.noreply.github.com>
2022-01-12 17:41:07 +0900

    modified/get-ssm-value-sh-actions

--- .github/actions/get_ssm_parameters_action/sh_action/entrypoint.sh
+++ .github/actions/get_ssm_parameters_action/sh_action/entrypoint.sh
@@ -28,10 +28,11 @@ do
     fi
 
     TARGET_KEY="/cd/${param}"
-    RESPONSE=$(aws ssm get-parameter --name "${TARGET_KEY}" --with-decryption)
-    VALUE="$(jq -r '.Parameter.Value' <(echo "${RESPONSE}"))"
+    # RESPONSE=$(aws ssm get-parameter --name "${TARGET_KEY}" --with-decryption)
+    RESPONSE=$(aws ssm get-parameter --name "${TARGET_KEY}" --with-decryption --query "Parameter.Value")
+    # VALUE="$(jq -r '.Parameter.Value' <(echo "${RESPONSE}"))"
 
-    SSM_PARAMETERS="${SSM_PARAMETERS}"\""${param}"\"\:\""${VALUE}"\""${END_STRING}"
+    SSM_PARAMETERS="${SSM_PARAMETERS}"\""${param}"\"\:\""${RESPONSE}"\""${END_STRING}"
     let i++
 done
 
@@ -46,16 +47,16 @@ do
     fi
 
     TARGET_KEY="/${ssm_path_name}/${env_name}/${param}"
-    RESPONSE=$(aws ssm get-parameter --name "${TARGET_KEY}" --with-decryption)
-    # RESPONSE=$(aws ssm get-parameter --name "${TARGET_KEY}" --with-decryption --query "Parameter.Value")
-    VALUE="$(jq -r '.Parameter.Value' <(echo "${RESPONSE}"))"
+    # RESPONSE=$(aws ssm get-parameter --name "${TARGET_KEY}" --with-decryption)
+    RESPONSE=$(aws ssm get-parameter --name "${TARGET_KEY}" --with-decryption --query "Parameter.Value")
+    # VALUE="$(jq -r '.Parameter.Value' <(echo "${RESPONSE}"))"
 
-    SSM_PARAMETERS="${SSM_PARAMETERS}"\""${param}"\"\:\""${VALUE}"\""${END_STRING}"
+    SSM_PARAMETERS="${SSM_PARAMETERS}"\""${param}"\"\:\""${RESPONSE}"\""${END_STRING}"
     let i++
 done
 
 # mask arg
-# echo "::add-mask::$SSM_PARAMETERS"
+echo "::add-mask::$SSM_PARAMETERS"
 
 # output arg
 echo "::set-output name=ssm_parameters::$SSM_PARAMETERS"
