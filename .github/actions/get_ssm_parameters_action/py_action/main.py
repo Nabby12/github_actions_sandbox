b223725e79007eca0283a6ad286c34077bfbbebf
Nabby12 <Nabby12@users.noreply.github.com>
2022-07-04 11:26:28 +0900

    added/if-statement-for-py-action

--- .github/actions/get_ssm_parameters_action/py_action/main.py
+++ .github/actions/get_ssm_parameters_action/py_action/main.py
@@ -10,35 +10,38 @@ ENV = os.environ['INPUT_ENV']
 CD_PARAMETERS = os.environ['INPUT_CD_PARAMETERS'].split('\n')
 PARAMETERS = os.environ['INPUT_PARAMETERS'].split('\n')
 
+
 def main():
     ssm_cd_path = '/cd/'
     ssm_path = '/' + SSM_PATH_NAME + '/' + ENV + '/'
 
     # 取得するパラメータストアのパス+名称を一つの配列に格納
     ssm_parameters = {}
-    for parameter in CD_PARAMETERS:
-        try: 
-            response = ssm.get_parameter(
-                Name = ssm_cd_path + parameter,
-                WithDecryption = True
-            )
-            ssm_parameters[parameter] = response['Parameter']['Value']
-            print('get ssm parameter succeeded.')
-        except Exception as err:
-            print(err)
-            raise Exception ('get ssm parameter failed.')
+    if CD_PARAMETERS:
+        for parameter in CD_PARAMETERS:
+            try:
+                response = ssm.get_parameter(
+                    Name=ssm_cd_path + parameter,
+                    WithDecryption=True
+                )
+                ssm_parameters[parameter] = response['Parameter']['Value']
+                print('get ssm parameter succeeded.')
+            except Exception as err:
+                print(err)
+                raise Exception('get ssm parameter failed.')
 
-    for parameter in PARAMETERS:
-        try: 
-            response = ssm.get_parameter(
-                Name = ssm_path + parameter,
-                WithDecryption = True
-            )
-            ssm_parameters[parameter] = response['Parameter']['Value']
-            print('get ssm parameter succeeded.')
-        except Exception as err:
-            print(err)
-            raise Exception ('get ssm parameter failed.')
+    if PARAMETERS:
+        for parameter in PARAMETERS:
+            try:
+                response = ssm.get_parameter(
+                    Name=ssm_path + parameter,
+                    WithDecryption=True
+                )
+                ssm_parameters[parameter] = response['Parameter']['Value']
+                print('get ssm parameter succeeded.')
+            except Exception as err:
+                print(err)
+                raise Exception('get ssm parameter failed.')
 
     # outputに定義する値をマスク
     for key in ssm_parameters:
@@ -46,6 +49,7 @@ def main():
 
     # outputに取得した値を定義
     print("::set-output name=ssm_parameters::" + json.dumps(ssm_parameters))
+
 
 if __name__ == '__main__':
     main()
